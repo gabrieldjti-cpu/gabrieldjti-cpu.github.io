7f750026312e40b6b7fa20e486086fd1b83ffa53
gabrieldjti-cpu <gabrieldj.ti@gmail.com>
2026-07-09 00:22:09 -0300

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+// Importa o cliente do Supabase
+const supabase = window.supabase.createClient(
+    "https://ikrsxmjrdnhyecjchjju.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrcnN4bWpyZG5oeWVjamNoamp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMDE5MjAsImV4cCI6MjA5MzY3NzkyMH0.7Pm_MR0nPwVhed0xV5ndvZX91EX-NS7DWZq5-5vF1Hg"
+);
