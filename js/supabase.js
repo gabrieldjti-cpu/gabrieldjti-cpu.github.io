5dea02c24d3e8a818e16689979204589e2fdab80
gabrieldjti-cpu <gabrieldj.ti@gmail.com>
2026-08-22 11:57:25 -0300

    feat: carrega módulo de estoque no painel da loja

--- js/supabase.js
+++ js/supabase.js
@@ -57,7 +57,7 @@ if (!window.supabase) {
 
         window.db = window.supabase.createClient(
             "https://ikrsxmjrdnhyecjchjju.supabase.co",
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrcnN4bWpyZG5oeWVjamNoamp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMDE5MjAsImV4cCI6MjA5MzY3NzkyMH0.7Pm_MR0nPwVhed0xV5ndvZX91EX-NS7DWZq5-5vF1Hg"
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJIUzI1NiIsInJlZiI6ImlrcnN4bWpyZG5oeWVjamNoamp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMDE5MjAsImV4cCI6MjA5MzY3NzkyMH0.7Pm_MR0nPwVhed0xV5ndvZX91EX-NS7DWZq5-5vF1Hg"
         );
 
     }
@@ -292,7 +292,8 @@ function carregarExtensoesDaPagina() {
                 "css/painel-loja-aprovacao.css",
 
             scripts: [
-                "js/painel-loja-aprovacao.js"
+                "js/painel-loja-aprovacao.js",
+                "js/painel-loja-estoque.js"
             ],
 
             iniciar:
